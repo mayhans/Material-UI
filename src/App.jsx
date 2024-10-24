6f00e1bc1163eb4aa4b924b6623eec571cd0c464
Mayra <mayra9820@yahoo.com>
2024-10-23 19:40:57 -0700

    beginning

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +29,17 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+      <Container sx={{ bgcolor: 'coral', height: '100vh', width: {
+          xs: '12',  // 0px and up
+          sm: '50%',   // 600px and up
+          md: '33.33%', // 900px and up
+        }
+        }}>
+        <Box sx={{ bgcolor: 'purple'}}>
+          Hello World
+        </Box>
+      </Container>
     </>
   )
 }
