dba37a626cca9c29002d717a4fa4f11ede13eafc
Mayra <mayra9820@yahoo.com>
2024-10-23 20:59:23 -0700

    messing around

--- src/App.jsx
+++ src/App.jsx
@@ -4,41 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import ButtonAppBar from './components/BasicAppBar/BasicAppBar';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
       <Container sx={{ bgcolor: 'coral', height: '100vh', width: {
-          xs: '12',  // 0px and up
-          sm: '50%',   // 600px and up
+          xs: '100%',  // 0px and up
+          sm: '100%',   // 600px and up
           md: '33.33%', // 900px and up
         }
         }}>
-        <Box sx={{ bgcolor: 'purple'}}>
-          Hello World
-        </Box>
+          <ButtonAppBar />
+        <Box sx={{ bgcolor: 'purple'}}></Box>
       </Container>
     </>
   )
