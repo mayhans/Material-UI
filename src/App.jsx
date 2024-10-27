e1a0df7e649d44f9edc1d9a55c15ea2c9ada1f3a
Mayra <mayra9820@yahoo.com>
2024-10-27 15:40:25 -0700

    figuring out break points

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
 import './App.css'
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import ButtonAppBar from './components/BasicAppBar/BasicAppBar';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Container sx={{ bgcolor: 'coral', height: '100vh', width: {
           xs: '100%',  // 0px and up
           sm: '100%',   // 600px and up
-          md: '33.33%', // 900px and up
+          md: '100%', // 900px and up
+          lg: '100%', // 1280px and up
+          xl: '100%', // 1920px and up
         }
         }}>
           <ButtonAppBar />
